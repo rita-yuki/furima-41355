3cc8d0c237c36df1fceeeed38203b507c9d93141
rita-snow <yummy.standrews@gmail.com>
2024-11-03 05:27:09 +0900

    新規登録/ユーザー情報の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
   def index
+  
     
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, match:(/@.+/)
+  validates :password, presence: true,confirmation: true, match: (/[a-z\d]{6,}/i) 
+  validates :password_confirmation, presence: true
+       
 end
