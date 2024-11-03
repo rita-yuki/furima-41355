8f9ce24c376b2b4673ad02b0c07cd921b64db009
rita-snow <yummy.standrews@gmail.com>
2024-11-03 15:44:58 +0900

    新規登録/本人情報確認の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, match:(/@.+/)
-  validates :password, presence: true,confirmation: true, match: (/[a-z\d]{6,}/i) 
+  validates :email, presence: true, uniqueness: true, format: { with: /\A.+@.+\z/, message: 'must contain an @ symbol' } 
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }, confirmation: true
   validates :password_confirmation, presence: true
-       
+
+  with_options presence: true do
+  validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+  validates :kana_family_name, format: { with: /\A[ァ-ヶ一ー]+\z/, message: '全角カタカナを使用してください' }
+  validates :kana_name, format: { with: /\A[ァ-ヶ一ー]+\z/, message: '全角カタカナを使用してください' }
+  validates :birthday
+  end
 end
