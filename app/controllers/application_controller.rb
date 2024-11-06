8a4b8d207cb09f66205603612ab7cae5b84b726b
rita-snow <yummy.standrews@gmail.com>
2024-11-06 21:46:19 +0900

    トップページの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,20 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :authenticate_user! 
+  # before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  #private
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
+  #def basic_auth
+   # authenticate_or_request_with_http_basic do |username, password|
+    #  username == 'admin' && password == '2222'
+   # end
+  #end
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up,  keys: [:nickname, :family_name, :name, :kana_family_name, :kana_name, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :name, :kana_family_name, :kana_name, :birthday])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 end
