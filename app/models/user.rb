5a67b3cf025022b324d15cf9690c5ad1ccc8d1ba
rita-snow <yummy.standrews@gmail.com>
2024-11-06 22:07:51 +0900

    エラーハンドリングの重複したエラーメッセージを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,8 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: /\A.+@.+\z/, message: 'must contain an @ symbol' } 
   
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }, confirmation: true
-  validates :password_confirmation, presence: true
+  PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/.freeze
+  validates :password, length: { minimum: 6 }, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }, confirmation: true
 
   with_options presence: true do
   validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
