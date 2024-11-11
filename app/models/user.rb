c262391739ba3a5a13d746c921aad453b74649c9
rita-snow <yummy.standrews@gmail.com>
2024-11-11 11:37:26 +0900

    ユーザー管理機能修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   validates :nickname, presence: true
 
   PASSWORD_REGEX = /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/
-  validates :password, format: { with: PASSWORD_REGEX, message: 'には半角英字と半角数字の両方を含めて設定してください' }, confirmation: true
+  validates :password, format: { with: PASSWORD_REGEX, message: 'には半角英字と半角数字の両方を含めて設定してください' }
 
   with_options presence: true do
     validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
