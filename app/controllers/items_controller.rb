4649c476e0f283d09a68e883e0b395b2b9205f12
rita-snow <yummy.standrews@gmail.com>
2024-11-13 11:17:33 +0900

    ルーティング・バリテーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
- before_action :authenticate_user!, only: [:index]
+ before_action :authenticate_user!, only: [:index, :new, :create]
+
   def index
     @items = Item.all
   end
@@ -8,4 +9,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+    redirect_to '/'
+    else
+     render 'new'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :description, :category_id, :product_status_id, :shipping_fee_id, :prefecture_id, :days_up_to_delivery_id, :price)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :product_status_id, presence: true
+  validates :shipping_fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_up_to_delivery_id, presence: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
+end
  
 end
