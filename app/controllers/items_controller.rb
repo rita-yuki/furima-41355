13752fc00a3ca2ad7afec66a064904805969796d
rita-snow <yummy.standrews@gmail.com>
2024-11-13 10:38:18 +0900

    ルーティング・コントローラーの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,11 @@
 class ItemsController < ApplicationController
  before_action :authenticate_user!, only: [:index]
   def index
+    @items = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
 end
