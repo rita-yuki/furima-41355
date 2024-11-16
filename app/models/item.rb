f06e242033786b61bf7c67e7390fd05a21194511
rita-snow <yummy.standrews@gmail.com>
2024-11-16 21:57:30 +0900

    エラーメッセージ関連の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,10 +2,17 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :product_status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :days_up_to_delivery
+
   validates :title, :description, presence: true
   validates :category_id, :product_status_id, :shipping_fee_id, :prefecture_id, :days_up_to_delivery_id, 
             presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
-            format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+            format: { with: /\A[0-9]+\z/, message: 'は数字で入力してください' }
   validates :image, presence: { message: 'を添付してください' }
 end
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Furima41355
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_storage.variant_processor = :mini_magick
 
