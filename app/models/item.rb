a84ef927ec67e986b46ff06ed05c4fc5702c2a8f
rita-snow <yummy.standrews@gmail.com>
2024-11-17 18:17:25 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,13 @@ class Item < ApplicationRecord
   belongs_to :days_up_to_delivery
 
   validates :title, :description, presence: true
-  validates :category_id, :product_status_id, :shipping_fee_id, :prefecture_id, :days_up_to_delivery_id, 
-            presence: true, numericality: { other_than: 1 , message: "can't be blank" } 
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
-            format: { with: /\A[0-9]+\z/, message: 'は数字で入力してください' }
+  validates :category_id, :product_status_id, :shipping_fee_id, :prefecture_id, :days_up_to_delivery_id,
+            presence: true, numericality: { other_than: 1, message: "can't be blank" }      
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'は半角数字で入力してください' }
+  validates :price, numericality: { 
+    only_integer: true, 
+    greater_than_or_equal_to: 300,
+    less_than_or_equal_to: 9_999_999
+  }
   validates :image, presence: { message: 'を添付してください' }
 end
