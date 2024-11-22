50962b63317f9302763e496f6711db4804068dc0
rita-snow <yummy.standrews@gmail.com>
2024-11-23 01:01:55 +0900

    モデルの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,10 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  def sold_out?
+    purchase.present?
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :product_status
