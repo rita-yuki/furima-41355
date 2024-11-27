292bc659f2ef88d2bae608f302dae8ae7ae43cbc
rita-snow <yummy.standrews@gmail.com>
2024-11-27 20:15:51 +0900

    rubocopによる修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,11 +46,11 @@ class ItemsController < ApplicationController
   def destroy
     @item = Item.find(params[:id])
     if current_user == @item.user
-     @item.destroy
+      @item.destroy
       redirect_to root_path
     else
-     redirect_to root_path
-    end 
+      redirect_to root_path
+    end
   end
 
   private
