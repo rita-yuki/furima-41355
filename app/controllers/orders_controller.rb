175fc65da3747a37c6fc6a8d4021704a71e105dd
rita-snow <yummy.standrews@gmail.com>
2024-12-06 06:04:07 +0900

    リファクタリングの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,6 +11,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
+      pay_item
       Payjp.api_key = 'sk_test_b572c80761b22c3659f971e7'
       Payjp::Charge.create(
         amount: @item.price,
@@ -42,4 +43,13 @@ class OrdersController < ApplicationController
       user_id: current_user.id, item_id: @item.id, token: params[:token]
     )
   end
+end
+
+def pay_item
+  Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+  Payjp::Charge.create(
+    amount: order_params[:price],  # 商品の値段
+    card: order_params[:token],    # カードトークン
+    currency: 'jpy'                 # 通貨の種類（日本円）
+  )
 end
