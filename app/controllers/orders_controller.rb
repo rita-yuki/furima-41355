022f94bc9352a2f7e9f6c8b6f401c2d16f745ed8
rita-snow <yummy.standrews@gmail.com>
2024-12-08 16:38:50 +0900

    環境変数の設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,7 @@ class OrdersController < ApplicationController
   before_action :redirect_if_seller_or_sold_out, only: [:index, :create]
 
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @prefectures = Prefecture.all
     @order_address = OrderAddress.new
   end
@@ -21,6 +22,7 @@ class OrdersController < ApplicationController
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       @prefectures = Prefecture.all
       render :index, status: :unprocessable_entity
     end
@@ -46,10 +48,10 @@ class OrdersController < ApplicationController
 end
 
 def pay_item
-  Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+  Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
   Payjp::Charge.create(
-    amount: order_params[:price],  # 商品の値段
-    card: order_params[:token],    # カードトークン
-    currency: 'jpy'                 # 通貨の種類（日本円）
+    amount: order_params[:price],  
+    card: order_params[:token],   
+    currency: 'jpy'                 
   )
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,6 @@
 const pay = () => {
-  const payjp = Payjp('pk_test_9b71807b2fa1c348ed87f96d');
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey)
   const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
@@ -30,3 +31,4 @@ const pay = () => {
 };
 
 window.addEventListener("turbo:load", pay);
+window.addEventListener("turbo:render", pay);
