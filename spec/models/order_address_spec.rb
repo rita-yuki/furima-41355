fa2415f7a0e02294000e9711aec045fd73f1d900
rita-snow <yummy.standrews@gmail.com>
2024-12-10 21:44:55 +0900

    テストコードの修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 RSpec.describe OrderAddress, type: :model do
   before do
-    @order_address = FactoryBot.build(:order_address)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item, user: @user)
+    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
   end
 
   context '内容に問題ない場合' do
     it 'すべての情報が正しく入力されていれば保存できる' do
+      expect(@order_address).to be_valid
+    end
+
+    it '建物名が空でも購入できる' do
+      @order_address.building_name = nil
       expect(@order_address).to be_valid
     end
   end
@@ -24,10 +31,16 @@ RSpec.describe OrderAddress, type: :model do
       expect(@order_address.errors.full_messages).to include('Postal code is invalid. Enter it as follows (e.g. 123-4567)')
     end
 
-    it 'prefecture_idが空では保存できない' do
-      @order_address.prefecture_id = nil
+    it 'prefecture_idが0では保存できない' do
+      @order_address.prefecture_id = 0
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+    end
+
+    it 'municipalityが空では購入できない' do
+      @order_address.municipality = nil
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include("Municipality can't be blank")
     end
 
     it 'blockが空では保存できない' do
@@ -46,6 +59,18 @@ RSpec.describe OrderAddress, type: :model do
       @order_address.telephone_number = '090123456'
       @order_address.valid?
       expect(@order_address.errors.full_messages).to include('Telephone number is too short')
+    end
+
+    it '電話番号が12桁以上だと購入できない' do
+      @order_address.telephone_number = '090123456789'
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include('Telephone number is invalid. Input only number')
+    end
+
+    it '電話番号が半角数字以外が含まれている場合は購入できない' do
+      @order_address.telephone_number = '090-1234-5678'
+      @order_address.valid?
+      expect(@order_address.errors.full_messages).to include('Telephone number is invalid. Input only number')
     end
 
     it 'tokenが空では保存できない' do
